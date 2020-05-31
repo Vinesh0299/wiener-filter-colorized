dcf8e8b3295f923d52f288dbf841d8c560187b4a
Vinesh <Vinesh.katewa@gmail.com>
2020-05-31 20:32:30 +0530

    Modified the patchify function

--- plow-filter.py
+++ plow-filter.py
@@ -13,17 +13,26 @@ def add_gaussian_noise(img, sigma):
 	return noisy_img
 
 # Function will return patches of default size (11,11)
-def patchify(img, patch_shape = 11):
-    return image.extract_patches_2d(img, (patch_shape, patch_shape))
+def patchify(img, patch_shape):
+    X, Y, Z = img.shape
+    x, y = patch_shape
+    shape = (X - x + 1, Y - y + 1, x, y)
+    X_str, Y_str, Z_str = img.strides
+    strides = (X_str, Y_str, X_str, Y_str)
+    return np.lib.stride_tricks.as_strided(img, shape=shape, strides=strides)
 
 if __name__ == '__main__':
     filename = os.path.join(os.getcwd(), './Noisy Image/photo.png')
+    gaussian_noise_image = os.path.join(os.getcwd(), './Noisy Image/gaussian_noise.png')
+
     myPhoto = io.imread(filename)
 
     # Code to create a gaussian noisy image
     #gaussian_noise = add_gaussian_noise(myPhoto, 20)
     #io.imsave('./Noisy Image/gaussian_noise.png', gaussian_noise)
 
+    gaussian_noise_image = io.imread(gaussian_noise_image)
+
     # Creating patches
-    #patches = patchify(myPhoto, 11)
-    #print(patches.shape)
+    patches = patchify(myPhoto, (11, 11))
+    print(patches.shape)
