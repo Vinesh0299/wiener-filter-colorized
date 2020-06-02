fe936a42f4ba6f0532f479295f8a44edf5574f27
Vinesh <Vinesh.katewa@gmail.com>
2020-06-02 21:06:58 +0530

    Implemented wiener filter for grayscale as well as rgb images

--- plow-filter.py
+++ plow-filter.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
+from numpy.fft import fft2, ifft2
 from skimage import io
 from skimage.transform import resize
-from sklearn.feature_extraction import image
+from scipy.signal import gaussian
 
 # Function will add gaussian noise to the image
 def add_gaussian_noise(img, sigma):
@@ -14,12 +15,34 @@ def add_gaussian_noise(img, sigma):
 
 # Function will return patches of default size (11,11)
 def patchify(img, patch_shape):
-    X, Y, Z = img.shape
+    X, Y = img.shape
     x, y = patch_shape
     shape = (X - x + 1, Y - y + 1, x, y)
-    X_str, Y_str, Z_str = img.strides
+    X_str, Y_str = img.strides
     strides = (X_str, Y_str, X_str, Y_str)
     return np.lib.stride_tricks.as_strided(img, shape=shape, strides=strides)
+
+# Function to convert RGB image to grayscale
+def rgb2gray(rgb):
+	return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+# Creates a gaussian kernel
+def gaussian_kernel(kernel_size = 3):
+	h = gaussian(kernel_size, kernel_size / 3).reshape(kernel_size, 1)
+	h = np.dot(h, h.transpose())
+	h /= np.sum(h)
+	return h
+
+# Wiener filter
+def wiener_filter(img, kernel, K):
+	kernel /= np.sum(kernel)
+	dummy = np.copy(img)
+	dummy = fft2(dummy)
+	kernel = fft2(kernel, s = img.shape)
+	kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
+	dummy = dummy * kernel
+	dummy = np.abs(ifft2(dummy))
+	return dummy
 
 if __name__ == '__main__':
     filename = os.path.join(os.getcwd(), './Noisy Image/photo.png')
@@ -33,6 +56,23 @@ if __name__ == '__main__':
 
     gaussian_noise_image = io.imread(gaussian_noise_image)
 
+    kernel = gaussian_kernel(3)
+
+    if(len(gaussian_noise_image.shape) == 3):
+        noisy_image = np.copy(gaussian_noise_image)
+        red_noise = noisy_image[:, :, 0]
+        green_noise = noisy_image[:, :, 1]
+        blue_noise = noisy_image[:, :, 2]
+
+        red_filtered = wiener_filter(red_noise, kernel, K=10)
+        green_filtered = wiener_filter(green_noise, kernel, K=10)
+        blue_filtered = wiener_filter(blue_noise, kernel, K=10)
+        filtered_image = np.dstack((red_filtered, green_filtered, blue_filtered))
+    else:
+        filtered_image = wiener_filter(gaussian_noise_image, kernel, K=10)
+
+    io.imsave('./Restored Images/restored.png', filtered_image)
+
     # Creating patches
-    patches = patchify(myPhoto, (11, 11))
-    print(patches.shape)
+    #patches = patchify(gaussian_noise_image, (11, 11))
+    #print(patches.shape)
